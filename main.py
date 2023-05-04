633d6d29456968fc5b2c9dc843f56a6ab8c4c789
codingwatermelon <jtorres7797@gmail.com>
2023-05-04 07:13:06 -0700

    Change variable get and file output

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 # Spotipy library reference https://spotipy.readthedocs.io/en/2.19.0/
-# See 'authorization code flow' section, export relevant env variables
 
-# This is a sample Python script.
+# Notes:
+# - Upon first execution, API will ask for authorization via web browser redirect URI. Paste the redirect site, then it will generate a .cache file so you don't have to do it again.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import spotipy
 import os
 import time
 import datetime
+import json
 from spotipy.oauth2 import SpotifyOAuth
+
+def get_vars():
+    data = json.load(open('config.json'))
+
+    return data['spotify_client_id'], data['spotify_client_secret'], data['spotify_client_redirect_uri']
 
 def get_user_library(scope, output):
 
-    client_id = os.environ.get('spotify_client_id')
-    client_secret = os.environ.get('spotify_client_secret')
-    client_redirect_uri = os.environ.get('spotify_client_redirect_uri')
+    client_id, client_secret, client_redirect_uri = get_vars()
 
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=client_redirect_uri))
 
     results = sp.current_user_saved_tracks(limit=50, offset=0)
    
-    f = open(output, "a")
+    f = open(output, "a", encoding="utf-8")
     
     i=0
     done = False
@@ -42,6 +44,7 @@ def get_user_library(scope, output):
                 i+=50
                 results = sp.current_user_saved_tracks(limit=50, offset=i)
             
+            # Add sleeps to throttle API requests (Spotify will get mad at me if not)
             time.sleep(0.25)
 
     f.close()
@@ -64,18 +67,15 @@ def get_user_top(scope):
 
     print(sp.current_user_top_artists(time_range='short_term', limit=50))
 
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     date = datetime.datetime.now()
-
-    scope = "user-library-read"
     #scope = "user-read-currently-playing"
 
     #scope = "user-top-read"
-    output= "/home/pi/spotipy/spotifyanalytics/allsongs_{}.txt".format(date.strftime("%Y_%m_%d"))
-    get_user_library(scope, output)
+
+    # where scope is "user-library-read"
+    get_user_library("user-library-read", "allsongs_{}.txt".format(date.strftime("%Y_%m_%d")))
+
     #get_currently_playing(scope)
     #get_user_top(scope)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
